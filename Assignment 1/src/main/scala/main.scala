1a4c7b6915fd2879e7fe98c8095674b4eccd0c32
ridhu-uic <rkungu2@uic.edu>
2022-09-14 00:38:53 -0500

    Draft 3

--- Assignment 1/src/main/scala/main.scala
+++ Assignment 1/src/main/scala/main.scala
@@ -1,32 +1,16 @@
 package scala
+import scala.collection.mutable.Map
 
-import scala.BoolExp.BooleanExpression.{NOT, Value}
+object Assignment:
+  var logicalGateMap : Map[String,Boolean] = scala.collection.mutable.Map()
 
+  var inputMap : Map[String,Boolean] = scala.collection.mutable.Map()
 
-/*
-//The operator Assign takes the specification of the logic gate and assigns it to a variable named logicGate1.
-Assign(LogicGate("logicGate1"), NOT(OR(Input("A"), Input("B"))))
-//test the created logic gate by specifying inputs for this logic gate
-//in this case the scope of the input variables is defined by the logic gate module/variable
-Scope(LogicGate("logicGate1"), Assign(Input("A"), true))
-Scope(LogicGate("logicGate1"), Assign(Input("B"), false))
-
-*/
-
-
-
- object BoolExp:
-
-
-   var logicGate1 : Boolean = false //(The variable is declared because only classes can have undeclared variable error)
-
-   def Assign(gate : String, expression: BooleanExpression) =
-     if gate=="logicGate1" then logicGate1=expression.eval
-     else print("The value of gate is not valid")
-
-   enum BooleanExpression:
-    case Value(v:Boolean)
-    case Input(x : String)
+  enum BooleanExpression:
+    case Value(v: Boolean)
+    case Variable(x: String)
+    case Input(x: String)
+    case logicGate(x:String)
     case NOT(o1: BooleanExpression)
     case OR(o1: BooleanExpression, o2: BooleanExpression)
     case AND(o1: BooleanExpression, o2: BooleanExpression)
@@ -35,33 +19,52 @@ Scope(LogicGate("logicGate1"), Assign(Input("B"), false))
     case XOR(o1: BooleanExpression, o2: BooleanExpression)
     case XNOR(o1: BooleanExpression, o2: BooleanExpression)
 
-    val EnvironmentTable: Map[String, Boolean] = Map("A" -> true, "B"->false)
-
     def eval: Boolean = this match
-      case Value(x:Boolean) => x
-      case Input(x : String) => EnvironmentTable.getOrElse(x.toString,throw new Exception(x))   // why it doesnt work without toString
+      case Value(x: Boolean) => x
+      case Variable(x: String) => logicalGateMap.getOrElse(x.toString, throw new Exception(x)) // why it doesnt work without toString
+      case Input(x: String) => inputMap.getOrElse(x.toString, throw new Exception(x))
+      case logicGate(x: String) => {print(x.toString)
+        logicalGateMap(x.toString)=false
+        println(logicalGateMap)
+        true}
       case NOT(o1) => !o1.eval
       case OR(o1, o2) => o1.eval | o2.eval
       case AND(o1, o2) => o1.eval & o2.eval
       case NAND(o1, o2) => !(o1.eval & o2.eval)
       case NOR(o1, o2) => !(o1.eval | o2.eval)
       case XOR(o1, o2) => o1.eval ^ o2.eval
-      case XNOR(o1,o2) => !(o1.eval ^ o2.eval)
+      case XNOR(o1, o2) => !(o1.eval ^ o2.eval)
 
+  def Assign(lg : scala.Assignment.BooleanExpression, valueExp : scala.Assignment.BooleanExpression) =
+    val value = valueExp.eval
+    print("logic gate expression ")
+    if lg.toString.matches("logicGate.*") == true then {
+      val Str1 = lg.toString
+      //println(Str)
+      //println(Str.length)
+      val gate = Str1.substring(10,Str1.length-1)
+      //print("gate = ")
+      //println(gate)
+      logicalGateMap(gate)=value
+      print("LogicGateMap is ")
+      println(logicalGateMap)
+    }
+    else if lg.toString.matches("Input.*") == true then {
+      val Str2 = lg.toString
+      println(Str2)
+      println(Str2.length)
+      val gate = Str2.substring(6, Str2.length - 1)
+      print("gate = ")
+      println(gate)
+      inputMap(gate) = value
+      print("inputMap is ")
+      println(inputMap)
+    }
+    else
+      print("else loop")
 
-
-
-
-   @main def runBoolExp =
+  @main def runIT =
     import BooleanExpression.*
-    //    Add(Add(Add(...)))
-    print("The value is : ")
-    //println( eval(XOR(Input("A"), NOT(Value(false)))))
-    println(NOT(Value(false)).eval)
-
-
-    print("The value of logicGate1 is ")
-    println(logicGate1)
-    Assign("logicGate1",XOR(Input("A"), NOT(Value(false))))
-    print("The value of logicGate1 after assign is ")
-    println(logicGate1)
+    Assign(Input("A"), Value(true))
+    Assign(Input("B"), Value(false))
+    println(Assign(logicGate("LogicGate1"), NOT(OR(Input("A"), Input("B")))))
