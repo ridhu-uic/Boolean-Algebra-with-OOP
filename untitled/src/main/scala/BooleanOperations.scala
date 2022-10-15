8c0fe30afb82378b3123a5e4e9d45df5e2cc211b
Ridhu <rkungu2@uic.edu>
2022-10-15 14:51:31 -0500

    Fields and Methods

---
+++ untitled/src/main/scala/BooleanOperations.scala
@@ -0,0 +1,181 @@
+import scala.collection.mutable.Stack
+
+object BooleanOperations:
+
+  val LogicGateMap: collection.mutable.Map[String, BooleanExpression] = collection.mutable.Map()
+  val inputGateMap: collection.mutable.Map[String, Map[String, Boolean]] = collection.mutable.Map()
+  val classMap: collection.mutable.Map[String, BooleanExpression.ClassDef] = collection.mutable.Map()
+  val fieldMap : collection.mutable.Map[String,collection.mutable.Map[String,BooleanExpression]] = collection.mutable.Map()
+  val methodMap : collection.mutable.Map[String,collection.mutable.Map[String,List[BooleanExpression]]] = collection.mutable.Map()
+
+  enum BooleanExpression:
+      //Declaring the Boolean Functions
+      case Input(name: String)
+      case get_Field_Object(name_object : String,name_field: String)
+      case get_Method_Object(name_object : String,name_field: String)
+      case get_Field(name_field: String)
+      case set_Field(name : String, value : BooleanExpression)
+      case invokeMethod(name_field: String)
+      case Field(name : String, value : BooleanExpression)
+      case Method(name : String, value : List[BooleanExpression])
+      case ClassDef(name : String, fields : List[Field],method : List[Method])
+      case NewObject(name : String, classType : ClassDef)
+      case printClassMap()
+      case Object(name : String, action : BooleanExpression)
+
+
+      case LogicGate(name: String)
+      //The input Value case is used to return the particular input value in the gate
+      case input_Value(l: LogicGate, c: String)
+      //The Value case is used to return the Boolean values.Its declared for the simplicity of code and will be removed in future
+      case Value(v: Boolean)
+      //The gate_Value is used to return the value of the LogicGate after evaluation
+      case gate_Value(l: BooleanExpression)
+      //NOT performs negation
+      case NOT(o1: BooleanExpression)
+      //OR performs Boolean OR operation
+      case OR(o1: BooleanExpression, o2: BooleanExpression)
+      //AND performs Boolean AND operation
+      case AND(o1: BooleanExpression, o2: BooleanExpression)
+      //NAND performs Boolean NAND operation
+      case NAND(o1: BooleanExpression, o2: BooleanExpression)
+      //NOR performs Boolean NOR operation
+      case NOR(o1: BooleanExpression, o2: BooleanExpression)
+      //XOR performs Boolean XOR operation
+      case XOR(o1: BooleanExpression, o2: BooleanExpression)
+      //XNOR performs Boolean XNOR operation
+      case XNOR(o1: BooleanExpression, o2: BooleanExpression)
+
+      case assign(input: BooleanExpression, value: BooleanExpression, gate: String = "default")
+
+
+      //scope is function used to give abstraction to the user.It helps in assigning the input values.
+      //The inputs to scope are LogicGate, Input and value of the input (datatype : BooleanExpression, eg: true is given as Value(true))
+      //The scopefunction will be improved in the future.
+      case scope(gate: LogicGate, inputC: Input, value: BooleanExpression)
+
+      case TestGate(gate: LogicGate, value: Boolean)
+
+      def classOperation : BooleanExpression = this match
+        case get_Method_Object(name_object : String,name_method: String) =>
+          Object(name_object,invokeMethod(name_method)).classOperation
+
+
+
+
+
+        case Object(name: String, action: BooleanExpression) => action match
+          case get_Field(name_field: String) =>
+            val x = fieldMap.get(name)
+            val y =x.get(name_field)
+            println("The value of "+name_field+"in Object "+name+"is :"+y)
+            y
+
+          case invokeMethod(name_method : String) =>
+            println(name_method+" method invoked.")
+            val x = methodMap.get(name)
+            val operations = x.get(name_method)
+            val result = Stack[BooleanExpression]()
+            println("operation")
+            println(operations)
+            for operation <-  operations do
+              operation match
+                case get_Field(name_field: String) =>
+                  println(operation)
+                  result.push(Object(name,get_Field(name_field)).classOperation)
+                case invokeMethod(name_method : String) =>
+                  result.push(get_Method_Object(name,name_method).classOperation)
+                case _ =>
+                  result.push(Value(operation.eval))
+            result.top
+
+
+
+
+
+        //case Method(name: String, value: BooleanExpression)
+        case NewObject(name: String, classType: ClassDef) =>
+          classType match
+            case ClassDef(name_class, fields, methods) =>
+              println("classdef matched")
+              println(fields)
+              val temp_fieldMap: collection.mutable.Map[String, BooleanExpression] = collection.mutable.Map()
+              for Field(name_field: String, value: BooleanExpression) <- fields do
+                temp_fieldMap.update(name_field, value)
+
+              fieldMap.update(name, temp_fieldMap)
+              println(fieldMap)
+              val temp_methodMap: collection.mutable.Map[String, List[BooleanExpression]] = collection.mutable.Map()
+
+              for Method(name_method: String, value: List[BooleanExpression]) <- methods do
+                temp_methodMap.update(name_method, value)
+              methodMap.update(name, temp_methodMap)
+              println(methodMap)
+              classType
+
+      def dataType: String = this match
+        case Input(name: String) => name
+
+        case LogicGate(name: String) => name
+        case _ =>
+          "Match not found for given datatype"
+
+      //Using eval to evaluate the Boolean Functions
+      def eval : Boolean = this match
+        case get_Field_Object(name_object: String, name_field: String) =>
+          val x = fieldMap.get(name_object)
+          val y=x.get(name_field)
+          y.eval
+        case printClassMap() =>
+          println(classMap)
+          true
+        case Value(x: Boolean) => x
+        case input_Value(l: LogicGate, c: String) => inputGateMap(l.name).getOrElse(c, false)
+        case NOT(o1) => !o1.eval
+        case OR(o1, o2) => o1.eval | o2.eval
+        case AND(o1, o2) => o1.eval & o2.eval
+        case NAND(o1, o2) => !(o1.eval & o2.eval)
+        case NOR(o1, o2) => !(o1.eval | o2.eval)
+        case XOR(o1, o2) => o1.eval ^ o2.eval
+        case XNOR(o1, o2) => !(o1.eval ^ o2.eval)
+        case gate_Value(gate: BooleanExpression) =>
+          gate match
+            case LogicGate(name: String) =>
+              val x = LogicGateMap.getOrElse(name, Value(false))
+              x.eval
+            case _ =>
+              println("Match not found for LogicGate : "+gate+" in gate_Value.")
+              false
+        case TestGate(gate: LogicGate, value: Boolean) =>
+          gate match
+            case LogicGate(name: String) =>
+              if (LogicGateMap.getOrElse(name, Value(false)).eval == value)
+                true
+              else
+                false
+
+
+        case assign(input: BooleanExpression, value: BooleanExpression, gate: String) =>
+          input match
+            case LogicGate(name: String) =>
+              LogicGateMap.put(name, value)
+              true
+            case Input(name) =>
+              print("Gate in scope->assign   :")
+              println(gate)
+              if !gate.matches("default") then
+                println("inside if")
+                inputGateMap.put(gate , Map(name -> value.eval))
+              else
+                println("else")
+              true
+            case _ =>
+              println("Match not found for input : "+input+" in assign.")
+              false
+
+        case scope(gate: BooleanExpression, inputC: BooleanExpression, value: BooleanExpression) =>
+          println("case scope")
+          assign(inputC, value, gate.dataType).eval
+          true
+
+
--- untitled/src/main/scala/main.scala
+++ untitled/src/main/scala/main.scala
@@ -1,163 +1,26 @@
+import BooleanOperations.{BooleanExpression, LogicGateMap, classMap, inputGateMap, methodMap, fieldMap  }
+import BooleanOperations.BooleanExpression.*
+
+import scala.collection.mutable.ListBuffer
 object main:
-  /*
-  * The object is to implement a domain-specific language (DSL) using Scala
-  *  for writing and evaluating set operation expressions for designers
-  *  of the digital logic to implement logic gates.
-  * */
-
-  //LogicGateMap and Input Map store the values of the LogicGate expressions and input values
-  //The LogicGateMap is a mutable map which holds the BooleanExpression that has to evaluated for finding the state of the
-  //corresponding LogicGate
-  val LogicGateMap : collection.mutable.Map[ String,BooleanExpression] =collection.mutable.Map()
-
-  /*
-  The inputGateMap is a mutable map which is of form [String,Map[String,Boolean]], it holds the input values
-  of the corresponding logic gates.
-
-  example of the value it  holds :: "logicGate1"->(A->true,B->false)
-
-  it means in logicGate1, two inputs are defined A and B with values true and False respectively.
 
 
-  */
-
-  val inputGateMap: collection.mutable.Map[String,Map[String, Boolean]] = collection.mutable.Map()
-
-  //A case class has be defined to hold the value of Input names and be a datatype for further usage in the model
-
-
-  //A case class has be defined to hold the value of Logicgate names and be a datatype for further usage in the model
-
-
-  // assign is a function used to assign value to LogicGates and their inputs.By default it assigns value to the LogicGate, only
-  //when the 3rd parameter is changed to some other value (name of the logicGate) it assign input values to the correponding
-  //logicGate. Note* BooleanExpression is an enumeration declared and discussed below in the file
-
-  /*TestGate is function that evaluates the value of the gate and compares it with the expected value.It returns true if it is equal else
-  returns false. Its inputs are LogicGate and Expected Boolean Value
-  * */
-  /*
-  def TestGate(gate : LogicGate,expected : Boolean) : Boolean =
-    val temp :BooleanExpression = LogicGateMap.getOrElse(gate.name,throw new Exception(gate.name))
-    if temp.eval == expected then true
-    else false
-  */
-  /*
-  * Boolean Expression is an enumeration used to define values.It has a function named eval to evaluate the corresponding cases
-  * */
-  enum BooleanExpression:
-    //Declaring the Boolean Functions
-    case Input(name : String)
-
-    case LogicGate(name : String)
-    //The input Value case is used to return the particular input value in the gate
-    case input_Value(l : LogicGate,c : String)
-    //The Value case is used to return the Boolean values.Its declared for the simplicity of code and will be removed in future
-    case Value(v: Boolean)
-    //The gate_Value is used to return the value of the LogicGate after evaluation
-    case gate_Value(l: BooleanExpression)
-    //NOT performs negation
-    case NOT(o1: BooleanExpression)
-    //OR performs Boolean OR operation
-    case OR(o1: BooleanExpression, o2: BooleanExpression )
-    //AND performs Boolean AND operation
-    case AND(o1: BooleanExpression, o2: BooleanExpression )
-    //NAND performs Boolean NAND operation
-    case NAND(o1: BooleanExpression, o2: BooleanExpression )
-    //NOR performs Boolean NOR operation
-    case NOR(o1: BooleanExpression , o2: BooleanExpression )
-    //XOR performs Boolean XOR operation
-    case XOR(o1: BooleanExpression , o2: BooleanExpression )
-    //XNOR performs Boolean XNOR operation
-    case XNOR(o1: BooleanExpression, o2: BooleanExpression )
-
-    case assign(input : BooleanExpression, value: BooleanExpression, gate: String = "default")
-
-    //scope is function used to give abstraction to the user.It helps in assigning the input values.
-    //The inputs to scope are LogicGate, Input and value of the input (datatype : BooleanExpression, eg: true is given as Value(true))
-    //The scopefunction will be improved in the future.
-    case scope(gate: LogicGate, inputC: Input, value: BooleanExpression)
-
-    case TestGate(gate : LogicGate, value : Boolean)
-
-    def dataType : String = this match
-      case Input(name: String) => name
-
-      case LogicGate(name: String) => name
-      case _ =>
-        "Match not found"
-
-
-    //Using eval to evaluate the Boolean Functions
-    def eval: Boolean = this match
-      case Value(x: Boolean) => x
-      case input_Value(l : LogicGate,c : String) => inputGateMap(l.name).getOrElse(c,false)
-      case NOT(o1) => !o1.eval
-      case OR(o1,o2) => o1.eval | o2.eval
-      case AND(o1, o2) => o1.eval & o2.eval
-      case NAND(o1, o2) => !(o1.eval & o2.eval)
-      case NOR(o1, o2) => !(o1.eval | o2.eval)
-      case XOR(o1, o2) => o1.eval ^ o2.eval
-      case XNOR(o1, o2) => !(o1.eval ^ o2.eval)
-      case gate_Value(gate : BooleanExpression) =>
-        gate match
-          case LogicGate(name : String) =>
-            val x = LogicGateMap.getOrElse(name,Value(false))
-            x.eval
-          case _ =>
-            println("Match not found")
-            false
-      case TestGate (gate : LogicGate, value : Boolean) =>
-        gate match
-          case LogicGate(name : String) =>
-            if(LogicGateMap.getOrElse(name,Value(false)).eval==value)
-              true
-            else
-              false
-
-
-
-
-
-      case assign(input: BooleanExpression, value: BooleanExpression, gate: String) =>
-        input match
-          case LogicGate(name : String) =>
-            LogicGateMap.put(name, value)
-            true
-          case Input(name) =>
-            print("Gate in scope->assign   :")
-            println(gate)
-            if !gate.matches("default") then
-                println("inside if")
-                inputGateMap.put(gate, Map(name -> value.eval))
-            else
-              println("else")
-            true
-          case _ =>
-            println("Match not found")
-            false
-
-
-      //scope is function used to give abstraction to the user.It helps in assigning the input values.
-      //The inputs to scope are LogicGate, Input and value of the input (datatype : BooleanExpression, eg: true is given as Value(true))
-      //The scopefunction will be improved in the future.
-      case scope(gate: BooleanExpression, inputC: BooleanExpression, value: BooleanExpression) =>
-        println("case scope")
-        assign(inputC, value, gate.dataType).eval
-        true
-
-      case _ =>
-        println("Match not found")
-        false
+  enum accessSpecifier:
+    case private_access
+    case public_access
+    case protected_access
 
 
 
   @main def runIT() : Unit =
-    import BooleanExpression.*
-
-    println(Input("Hello").dataType)
-    println(XOR(NOT(Value(false)),Value(false)).eval)
-
+    NewObject("A",ClassDef("example",List(Field("A",Value(false)),Field("B",Value(true))),List(Method("One",List(XOR(NOT(Value(false)),Value(true)),NOT(get_Field_Object("A","B")))),Method("two",List(XOR(NOT(Value(true)),Value(true)))),Method("three",List(invokeMethod("One"),get_Field("B")))))).classOperation
+    //println(classMap)
+    println(methodMap)
+    println(Object("A",get_Field("A")).classOperation)
+    println("method invoking")
+    println("The result of method One in object A is :" + Object("A", invokeMethod("One")).classOperation)
+    println("The result of method One in object A is :" + Object("A",invokeMethod("three")).classOperation)
+    /*
     //assign is used to assign the BooleanExpression to the logicGate1
     assign(LogicGate("logicGate1"),XOR(Value(true),input_Value(LogicGate("logicGate1"),"A"))).eval
     scope(LogicGate("logicGate1"),Input("A"),Value(true)).eval
@@ -194,3 +57,4 @@ object main:
     // False must be returned as the value of C is false but the expected mentioned in TestGate is true
     println(TestGate(LogicGate("logicGate4"),true).eval)
     println(gate_Value(LogicGate("logicGate4")).eval)
+    */
